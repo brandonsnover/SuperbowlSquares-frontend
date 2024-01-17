800ce1fcfd4057d671e18f5cb4c23fa958d158f0
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2024-01-17 16:10:55 -0600

    creates grid and populates with sqaure data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { Grid } from "./Grid";
+import axios from "axios";
+
 export function Content() {
+  const [squares, setSquares] = useState([]);
+
+  const handleIndexSquares = () => {
+    axios.get("http://localhost:3000/squares.json", { params: { grid_id: 14 } }).then((response) => {
+      console.log(response.data);
+      setSquares(response.data);
+    });
+  };
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <Grid squares={squares} onIndexSquares={handleIndexSquares} />
     </main>
   );
 }
