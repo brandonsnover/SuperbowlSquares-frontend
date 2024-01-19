64f3a5b3fb2ff7ed54cab06f0f86ebcc28245296
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-01-19 14:31:37 -0500

    fixed new grid form

--- src/GridsIndex.jsx
+++ src/GridsIndex.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 export function GridsIndex(props) {
   const navigate = useNavigate();
+  const [newGrid, setNewGrid] = useState();
+
   const goToGrid = (id) => {
     navigate(`/grid/${id}`);
+  };
+
+  const handleCreateGrid = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:3000/grids.json", { name: newGrid }).then((response) => {
+      console.log("grid create", response.data);
+      setNewGrid(response.data);
+      navigate(`/grid/${response.data.id}`);
+    });
   };
 
   return (
@@ -11,10 +24,18 @@ export function GridsIndex(props) {
       <h1>All Grids</h1>
       {props.grids.map((grid) => (
         <div key={grid.id}>
-          <h3>{grid.code}</h3>
-          <button onClick={() => goToGrid(grid.id)}>Go To Grid</button>
+          <h3>Grid: {grid.name}</h3>
+          <button onClick={() => goToGrid(grid.id)}>Go To {grid.name}</button>
         </div>
       ))}
+      <form onSubmit={handleCreateGrid}>
+        <h4>Make a new grid</h4>
+        <div>
+          {" "}
+          Name: <input type="text" name="name" onChange={(e) => setNewGrid(e.target.value)} required />
+          <button type="submit">Create </button>
+        </div>
+      </form>
     </div>
   );
 }
