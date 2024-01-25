f3350f586c9b53db993bd8d7afe313b86e9839dc
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-01-25 18:24:53 -0500

    fixed issues with flexbox

--- src/GridsIndex.jsx
+++ src/GridsIndex.jsx
@@ -20,33 +20,43 @@ export function GridsIndex(props) {
   };
 
   return (
-    <div>
-      <h4>Enter grid code:</h4>
-      <div>
-        <input type="text" name="code" />
-        <button type="submit">Go </button>
-      </div>
-      <h1>Your Grids</h1>
+    <div className="lists-container">
+      <h1 className="grids-with-squares">Grids With Squares</h1>
       {props.hasSquareGrids.map((grid) => (
         <div key={grid.id}>
-          <h3>Grid: {grid.name}</h3>
-          <button onClick={() => goToGrid(grid.id)}>Go To {grid.name}</button>
+          <h2>Grid: {grid.name}</h2>
+          <h4>Invite Code: {grid.code}</h4>
+          <button style={{ marginLeft: 10 }} onClick={() => goToGrid(grid.id)}>
+            Go To {grid.name}
+          </button>
         </div>
       ))}
-      <h1>Owned Grids</h1>
+      <hr width="100%" size="2"></hr>
+      <h1 className="owned-grids">Owned Grids</h1>
       {props.ownedGrids.map((grid) => (
         <div key={grid.id}>
-          <h3>Grid: {grid.name}</h3>
-          <h3>Code: {grid.code}</h3>
-          <button onClick={() => goToGrid(grid.id)}>Go To {grid.name}</button>
-          <button onClick={() => props.onDeleteGrid(grid)}>Delete Grid</button>
+          <h2>Grid: {grid.name}</h2>
+          <h4>Invite Code: {grid.code}</h4>
+          <button style={{ marginLeft: 10 }} onClick={() => goToGrid(grid.id)}>
+            Go To {grid.name}
+          </button>
+          <button style={{ marginLeft: 10 }} onClick={() => props.onDeleteGrid(grid)}>
+            Delete Grid
+          </button>
         </div>
       ))}
       <form onSubmit={handleCreateGrid}>
-        <h4>Make a new grid</h4>
+        <h4>Create your own</h4>
         <div>
           {" "}
-          Name: <input type="text" name="name" onChange={(e) => setNewGrid(e.target.value)} required />
+          Name:{" "}
+          <input
+            type="text"
+            name="name"
+            placeholder=" Grid Name "
+            onChange={(e) => setNewGrid(e.target.value)}
+            required
+          />
           <button type="submit">Create </button>
         </div>
       </form>
