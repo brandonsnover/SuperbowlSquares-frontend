15ff0531714a0361a2344d459f35f56971e5ec0d
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-02-09 10:55:59 -0500

    changed header and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import taylorSwift from "./assets/taylor.jpg";
+
 export function Footer() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <footer>
-      <p style={{ textAlign: "center" }}> 🏈 </p>
+    <footer
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ textAlign: "center" }}
+    >
+      {isHovered ? <img src={taylorSwift} alt="Taylor Swift" style={{ width: "18px", height: "18px" }} /> : <p> 🏈 </p>}
     </footer>
   );
 }
