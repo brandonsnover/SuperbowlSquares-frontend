b1ae87ba488e9ffaea2cb8c3d7c36ceedbf6431a
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-02-16 17:34:39 -0500

    fixed api requests and ot scenario

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -146,8 +146,58 @@ export function Grid(props) {
   }, [props.squares]);
 
   //Updating points squares
+  // const fetchAndSaveScores = async () => {
+  //   // Fetch scores from the API
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLBoxScore",
+  //     params: { gameID: "20240211_SF@KC" }, //20240121_KC@BUF,20240211_SF@KC
+  //     headers: {
+  //       "X-RapidAPI-Key": import.meta.env.VITE_APP_NFL_TANK01_API_KEY,
+  //       "X-RapidAPI-Host": "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com",
+  //     },
+  //   };
+
+  //   try {
+  //     const response = await axios.request(options);
+  //     // Check if the game hasn't started yet based on the API response
+  //     if (
+  //       response.data.body &&
+  //       response.data.body.error &&
+  //       response.data.body.error.includes("Game hasn't started yet")
+  //     ) {
+  //       console.log("Game hasn't started yet. It will start at 6:30 PM (ET).");
+  //       return;
+  //       // Exit the function early if the game hasn't started
+  //     }
+  //     const lineScores = response.data.body.lineScore;
+  //     // Assume response structure is as described and contains lineScore for home and away
+
+  //     // Map lineScores to your rails expected format
+  //     const scoresToUpdate = [
+  //       { id: 1, points: lineScores.home.Q1 || "0" },
+  //       { id: 2, points: lineScores.home.Q2 || "0" },
+  //       { id: 3, points: lineScores.home.Q3 || "0" },
+  //       { id: 4, points: lineScores.home.Q4 || "0" },
+  //       { id: 5, points: lineScores.away.Q1 || "0" },
+  //       { id: 6, points: lineScores.away.Q2 || "0" },
+  //       { id: 7, points: lineScores.away.Q3 || "0" },
+  //       { id: 8, points: lineScores.away.Q4 || "0" },
+  //     ];
+
+  //     // Update scores in the backend
+  //     scoresToUpdate.forEach((score) => {
+  //       axios
+  //         .patch(`${import.meta.env.VITE_APP_API_URL}/scores/${score.id}.json`, { points: score.points })
+  //         .then((response) => console.log(`Updated score ID ${score.id}`, response.data))
+  //         .catch((error) => console.error(`Error updating score ID ${score.id}`, error));
+  //     });
+  //   } catch (error) {
+  //     console.error("Error fetching scores from API", error);
+  //   }
+  // };
+
   const fetchAndSaveScores = async () => {
-    // Fetch scores from the API
     const options = {
       method: "GET",
       url: "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLBoxScore",
@@ -160,38 +210,55 @@ export function Grid(props) {
 
     try {
       const response = await axios.request(options);
-      // Check if the game hasn't started yet based on the API response
-      if (
-        response.data.body &&
-        response.data.body.error &&
-        response.data.body.error.includes("Game hasn't started yet")
-      ) {
-        console.log("Game hasn't started yet. It will start at 6:30 PM (ET).");
-        return;
-        // Exit the function early if the game hasn't started
+      if (response.data.statusCode === 200 && response.data.body && response.data.body.lineScore) {
+        const { home, away } = response.data.body.lineScore;
+
+        // Fetch existing scores to determine create or update
+        const existingScoresResponse = await axios.get(`${import.meta.env.VITE_APP_API_URL}/scores.json`);
+        const existingScores = existingScoresResponse.data;
+
+        // Function to decide posting or patching scores for a team
+        const decidePostOrPatchScoresForTeam = async (team, location) => {
+          const periods = ["Q1", "Q2", "Q3", "Q4"].concat(team.OT ? ["OT"] : []);
+
+          for (const period of periods) {
+            if (team[period]) {
+              const existingScore = existingScores.find(
+                (score) => score.quarter === period && score.location === location
+              );
+              const scoreData = {
+                quarter: period,
+                location: location,
+                points: team[period],
+              };
+
+              if (existingScore) {
+                // Patch existing score
+                try {
+                  await axios.patch(`${import.meta.env.VITE_APP_API_URL}/scores/${existingScore.id}.json`, scoreData);
+                  console.log(`Updated ${location} ${period} score: ${team[period]}`);
+                } catch (error) {
+                  console.error(`Error updating ${location} ${period} score`, error);
+                }
+              } else {
+                // Post new score
+                try {
+                  await axios.post(`${import.meta.env.VITE_APP_API_URL}/scores.json`, scoreData);
+                  console.log(`Posted new ${location} ${period} score: ${team[period]}`);
+                } catch (error) {
+                  console.error(`Error posting new ${location} ${period} score`, error);
+                }
+              }
+            }
+          }
+        };
+
+        // Decide to post or patch scores for home and away teams
+        await decidePostOrPatchScoresForTeam(home, "home");
+        await decidePostOrPatchScoresForTeam(away, "away");
+      } else {
+        console.log("Game data is not in the expected format or not completed.");
       }
-      const lineScores = response.data.body.lineScore;
-      // Assume response structure is as described and contains lineScore for home and away
-
-      // Map lineScores to your rails expected format
-      const scoresToUpdate = [
-        { id: 1, points: lineScores.home.Q1 || "0" },
-        { id: 2, points: lineScores.home.Q2 || "0" },
-        { id: 3, points: lineScores.home.Q3 || "0" },
-        { id: 4, points: lineScores.home.Q4 || "0" },
-        { id: 5, points: lineScores.away.Q1 || "0" },
-        { id: 6, points: lineScores.away.Q2 || "0" },
-        { id: 7, points: lineScores.away.Q3 || "0" },
-        { id: 8, points: lineScores.away.Q4 || "0" },
-      ];
-
-      // Update scores in the backend
-      scoresToUpdate.forEach((score) => {
-        axios
-          .patch(`${import.meta.env.VITE_APP_API_URL}/scores/${score.id}.json`, { points: score.points })
-          .then((response) => console.log(`Updated score ID ${score.id}`, response.data))
-          .catch((error) => console.error(`Error updating score ID ${score.id}`, error));
-      });
     } catch (error) {
       console.error("Error fetching scores from API", error);
     }
@@ -200,7 +267,7 @@ export function Grid(props) {
   //sets tank01 api request to be made on feb 11 6:30 every 20 minutes for 5 hours
   useEffect(() => {
     const now = new Date();
-    const startDateTime = new Date("2024-02-11T18:30:00-05:00");
+    const startDateTime = new Date("2024-01-11T18:30:00-05:00");
     // 'YYYY-MM-DDTHH:MM:SS-05:00' for Eastern Time
     if (now > startDateTime) {
       // If it's past the start time, adjust the year for the next occurrence
@@ -208,8 +275,8 @@ export function Grid(props) {
     }
     const delayUntilStart = startDateTime - now;
 
-    const fetchInterval = 20 * 60 * 1000;
-    // 20 minutes in milliseconds
+    const fetchInterval = 30 * 60 * 1000;
+    // 30 minutes in milliseconds
     const duration = 5 * 60 * 60 * 1000;
     // 5 hours in milliseconds
     let intervalId;
@@ -230,36 +297,93 @@ export function Grid(props) {
       clearInterval(intervalId);
     };
   }, []);
+
+  // const [homeScore, setHomeScore] = useState({ Q1: 0, Q2: 0, Q3: 0, Q4: 0, totalPts: 0 });
+  // const [awayScore, setAwayScore] = useState({ Q1: 0, Q2: 0, Q3: 0, Q4: 0, totalPts: 0 });
+
+  // //updates scores table if possible
+  // useEffect(() => {
+  //   axios
+  //     .get(`${import.meta.env.VITE_APP_API_URL}/scores.json`)
+  //     .then((response) => {
+  //       const scores = response.data;
+  //       // Assuming scores is an array with a specific order
+  //       // and the first 4 are home Q1 to Q4 and the next 4 are away Q1 to Q4
+  //       setHomeScore({
+  //         Q1: scores[0]?.points || 0,
+  //         Q2: scores[1]?.points || 0,
+  //         Q3: scores[2]?.points || 0,
+  //         Q4: scores[3]?.points || 0,
+  //         totalPts: scores.slice(0, 4).reduce((acc, curr) => acc + (curr?.points || 0), 0),
+  //       });
+  //       setAwayScore({
+  //         Q1: scores[4]?.points || 0,
+  //         Q2: scores[5]?.points || 0,
+  //         Q3: scores[6]?.points || 0,
+  //         Q4: scores[7]?.points || 0,
+  //         totalPts: scores.slice(4, 8).reduce((acc, curr) => acc + (curr?.points || 0), 0),
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching scores", error);
+  //     });
+  // }, []);
+
+  //new function try feb 16
+
   const [homeScore, setHomeScore] = useState({ Q1: 0, Q2: 0, Q3: 0, Q4: 0, totalPts: 0 });
   const [awayScore, setAwayScore] = useState({ Q1: 0, Q2: 0, Q3: 0, Q4: 0, totalPts: 0 });
 
-  //updates scores table if possible
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_APP_API_URL}/scores.json`)
       .then((response) => {
         const scores = response.data;
-        // Assuming scores is an array with a specific order
-        // and the first 4 are home Q1 to Q4 and the next 4 are away Q1 to Q4
-        setHomeScore({
-          Q1: scores[0]?.points || 0,
-          Q2: scores[1]?.points || 0,
-          Q3: scores[2]?.points || 0,
-          Q4: scores[3]?.points || 0,
-          totalPts: scores.slice(0, 4).reduce((acc, curr) => acc + (curr?.points || 0), 0),
+        let homeScoreUpdate = { ...homeScore };
+        let awayScoreUpdate = { ...awayScore };
+
+        scores.forEach((score) => {
+          const { location, quarter, points } = score;
+          let targetScore = location === "home" ? homeScoreUpdate : awayScoreUpdate;
+
+          if (quarter.startsWith("OT")) {
+            // Handle OT quarters by dynamically adding them if they don't exist
+            if (!targetScore[quarter]) {
+              targetScore[quarter] = 0; // Initialize OT period if it doesn't exist
+            }
+          }
+
+          if (quarter && points !== undefined) {
+            targetScore[quarter] = points; // Update the quarter or OT period points
+          }
         });
-        setAwayScore({
-          Q1: scores[4]?.points || 0,
-          Q2: scores[5]?.points || 0,
-          Q3: scores[6]?.points || 0,
-          Q4: scores[7]?.points || 0,
-          totalPts: scores.slice(4, 8).reduce((acc, curr) => acc + (curr?.points || 0), 0),
-        });
+
+        // Calculate total points for Home and Away, including OT
+        homeScoreUpdate.totalPts =
+          Object.values(homeScoreUpdate).reduce((acc, curr) => acc + curr, 0) - homeScore.totalPts;
+        awayScoreUpdate.totalPts =
+          Object.values(awayScoreUpdate).reduce((acc, curr) => acc + curr, 0) - awayScore.totalPts;
+
+        // Update state
+        setHomeScore(homeScoreUpdate);
+        setAwayScore(awayScoreUpdate);
       })
       .catch((error) => {
         console.error("Error fetching scores", error);
       });
   }, []);
+
+  // Function to dynamically generate OT periods headers
+  const renderOTHeaders = () => {
+    const otPeriods = Object.keys(homeScore).filter((key) => key.startsWith("OT"));
+    return otPeriods.map((ot) => <th key={ot}>{ot}</th>);
+  };
+
+  // Function to dynamically generate OT periods scores for a team
+  const renderOTScores = (teamScore) => {
+    const otPeriods = Object.keys(teamScore).filter((key) => key.startsWith("OT"));
+    return otPeriods.map((ot) => <td key={ot}>{teamScore[ot]}</td>);
+  };
 
   //new logic for winning squares
   const [winningSquare1, setWinningSquare1] = useState(null);
@@ -385,6 +509,7 @@ export function Grid(props) {
                   <th>Q2</th>
                   <th>Q3</th>
                   <th>Q4</th>
+                  {renderOTHeaders()}
                   <th>Total</th>
                 </tr>
               </thead>
@@ -395,6 +520,7 @@ export function Grid(props) {
                   <td>{homeScore.Q2}</td>
                   <td>{homeScore.Q3}</td>
                   <td>{homeScore.Q4}</td>
+                  {renderOTScores(homeScore)}
                   <td>{homeScore.totalPts}</td>
                 </tr>
                 <tr>
@@ -403,6 +529,7 @@ export function Grid(props) {
                   <td>{awayScore.Q2}</td>
                   <td>{awayScore.Q3}</td>
                   <td>{awayScore.Q4}</td>
+                  {renderOTScores(awayScore)}
                   <td>{awayScore.totalPts}</td>
                 </tr>
               </tbody>
